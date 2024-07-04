4ee49b2beadcd393070a8624c7c949c869bd7aa6
WinstonMDP <skrimer.swat@bk.ru>
2024-07-04 21:48:38 +0300

    Fix the Alarm to be movable between tasks

--- src/io.rs
+++ src/io.rs
@@ -1,13 +1,14 @@
 use std::{
     future::Future,
     pin::Pin,
-    task::{Context, Poll},
+    task::{Context, Poll, Waker},
     thread::{self, sleep},
     time::{Duration, Instant},
 };
 
 pub struct Alarm {
     instant: Instant,
+    waker: Option<Waker>,
 }
 
 impl Alarm {
@@ -15,6 +16,7 @@ impl Alarm {
     pub fn timer(secs: u64) -> Alarm {
         Alarm {
             instant: Instant::now() + Duration::new(secs, 0),
+            waker: None,
         }
     }
 }
@@ -22,17 +24,21 @@ impl Alarm {
 impl Future for Alarm {
     type Output = ();
 
-    fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+    fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
         if Instant::now() >= self.instant {
             println!("Done");
             return Poll::Ready(());
         }
         let waker = cx.waker().clone();
-        let instant = self.instant;
-        thread::spawn(move || {
-            sleep(instant - Instant::now());
-            waker.wake();
-        });
+        if self.waker.is_some() {
+            self.waker = Some(waker);
+        } else {
+            let instant = self.instant;
+            thread::spawn(move || {
+                sleep(instant - Instant::now());
+                waker.wake();
+            });
+        }
         Poll::Pending
     }
 }
@@ -41,11 +47,33 @@ impl Future for Alarm {
 mod tests {
     use super::Alarm;
     use crate::rt::Rt;
+    use std::{
+        future::Future,
+        pin::Pin,
+        sync::Arc,
+        task::{Context, Wake, Waker},
+    };
 
     #[test]
     fn alarm_t_1() {
         let rt = Rt::new();
         rt.spawner().spawn(Alarm::timer(3));
+        rt.run();
+    }
+
+    struct Empty {}
+
+    impl Wake for Empty {
+        fn wake(self: std::sync::Arc<Self>) {}
+    }
+
+    #[test]
+    fn alarm_t_2() {
+        let rt = Rt::new();
+        let mut alarm = Alarm::timer(3);
+        let _future =
+            Pin::new(&mut alarm).poll(&mut Context::from_waker(&Waker::from(Arc::new(Empty {}))));
+        rt.spawner().spawn(alarm);
         rt.run();
     }
 }
