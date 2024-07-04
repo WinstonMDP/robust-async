d7984e3c9870e9b612cadb23ded5b97d91f1bfee
WinstonMDP <skrimer.swat@bk.ru>
2024-07-04 17:59:21 +0300

    Update the AsyncMutex to Sync and remove lock

--- src/sync.rs
+++ src/sync.rs
@@ -4,71 +4,67 @@ use std::{
     future::Future,
     ops::{Deref, DerefMut},
     pin::Pin,
+    sync::Mutex,
     task::{Context, Poll, Waker},
 };
 
-pub struct AsyncMutex<T>(UnsafeCell<InnerAsyncMutex<T>>);
+pub struct AsyncMutex<T> {
+    t: UnsafeCell<T>,
+    lock: Mutex<bool>,
+    wakers: Mutex<VecDeque<Waker>>,
+}
 
 impl<T> AsyncMutex<T> {
     pub fn new(t: T) -> Self {
-        Self(UnsafeCell::new(InnerAsyncMutex {
-            t,
-            lock: false,
-            wakers: VecDeque::new(),
-        }))
-    }
-
-    #[must_use]
-    pub fn lock(&self) -> AsyncLock<T> {
-        AsyncLock(self)
+        Self {
+            t: t.into(),
+            lock: false.into(),
+            wakers: VecDeque::new().into(),
+        }
     }
 }
 
-struct InnerAsyncMutex<T> {
-    t: T,
-    lock: bool,
-    wakers: VecDeque<Waker>,
-}
+unsafe impl<T: Send> Sync for AsyncMutex<T> {}
 
-pub struct AsyncLock<'a, T>(&'a AsyncMutex<T>);
-
-impl<'a, T> Future for AsyncLock<'a, T> {
+impl<'a, T> Future for &'a AsyncMutex<T> {
     type Output = AsyncMutexGuard<'a, T>;
 
     fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
-        if !unsafe { &*self.0 .0.get() }.lock {
-            unsafe { &mut *self.0 .0.get() }.lock = true;
-            return Poll::Ready(AsyncMutexGuard(AsyncLock(self.0)));
+        let mut lock = self.lock.lock().unwrap();
+        if !*lock {
+            *lock = true;
+            return Poll::Ready(AsyncMutexGuard(&self));
         }
-        unsafe { &mut *self.0 .0.get() }
-            .wakers
+        self.wakers
+            .try_lock()
+            .unwrap()
             .push_back(cx.waker().clone());
         Poll::Pending
     }
 }
 
-pub struct AsyncMutexGuard<'a, T>(AsyncLock<'a, T>);
+pub struct AsyncMutexGuard<'a, T>(&'a AsyncMutex<T>);
 
 impl<T> Deref for AsyncMutexGuard<'_, T> {
     type Target = T;
 
     fn deref(&self) -> &Self::Target {
-        &unsafe { &*self.0 .0 .0.get() }.t
+        unsafe { &*self.0.t.get() }
     }
 }
 
 impl<T> DerefMut for AsyncMutexGuard<'_, T> {
     fn deref_mut(&mut self) -> &mut Self::Target {
-        &mut unsafe { &mut *self.0 .0 .0.get() }.t
+        unsafe { &mut *self.0.t.get() }
     }
 }
 
 impl<T> Drop for AsyncMutexGuard<'_, T> {
     fn drop(&mut self) {
-        if let Some(waker) = unsafe { &mut *self.0 .0 .0.get() }.wakers.pop_front() {
+        if let Some(waker) = self.0.wakers.try_lock().unwrap().pop_front() {
             waker.wake();
         }
-        unsafe { &mut *self.0 .0 .0.get() }.lock = false;
+        *self.0.lock.try_lock().unwrap() = false;
     }
 }
 
@@ -87,11 +83,11 @@ mod tests {
         let c_v = v.clone();
         rt.spawner().spawn(async move {
             let async_mutex = async_mutex.clone();
-            let _guard = async_mutex.lock().await;
+            let _guard = async_mutex.as_ref().await;
             let async_mutex = async_mutex.clone();
             let c_c_v = c_v.clone();
             spawner.spawn(async move {
-                async_mutex.lock().await;
+                async_mutex.as_ref().await;
                 c_c_v.borrow_mut().push(1);
             });
             Alarm::timer(3).await;
